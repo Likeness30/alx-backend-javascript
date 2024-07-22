b025ccc22d537ebb9a78d3f441df332b68679cb1
Rufus <likeness14@gmail.com>
2024-07-22 19:05:58 +0100

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    const task = true; // eslint-disable-line no-unused-var
+    const task2 = false; // eslint-disable-line no-unused-var
   }
 
   return [task, task2];
