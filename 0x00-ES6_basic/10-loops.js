6f72308978c2275a7e24ef55b2edc1a12fdb399d
Rufus <likeness14@gmail.com>
2024-07-23 16:13:50 +0100

    10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const [index, value] of array.entries()) {
-    // eslint-disable-next-line no-param-reassign
-    array[index] = appendString + value;
+  const newArray = [];
+
+  for (const value of array) {
+    newArray.push(appendString + value);
   }
 
-  return array;
+  return newArray;
 }
