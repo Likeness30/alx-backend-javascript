cd21673ed8a80f0fc31a5fea48b356bf80311187
Rufus <likeness14@gmail.com>
2024-07-24 19:36:05 +0100

    1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,4 +1,4 @@
-function getFullResponseFromAPI(success) {
+export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
       resolve({ status: 200, body: 'Success' });
@@ -9,5 +9,3 @@ function getFullResponseFromAPI(success) {
     }
   });
 }
-
-export default getFullResponseFromAPI;
