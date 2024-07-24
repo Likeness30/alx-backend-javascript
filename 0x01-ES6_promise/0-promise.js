7e91e81b1a1bef360d28b5f2b9516122cb4a4600
Rufus <likeness14@gmail.com>
2024-07-24 18:51:48 +0100

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,12 +1,5 @@
-function getResponseFromAPI() {
-  return new Promise((resolve) => {
-    // Simulate an asynchronous operation
-    setTimeout(() => {
-      // For instance lets resolve the promise with some data
-      const data = { message: 'API call sucessful' };
-      resolve(data);
-    }, 1000); // Time delayed for 1 seconds
-  });
-}
-
-export default getResponseFromAPI;
+/* eslint-disable */
+export default function getResponseFromAPI() {
+    return (new Promise((resolve, reject) => {}));
+  }
+  
