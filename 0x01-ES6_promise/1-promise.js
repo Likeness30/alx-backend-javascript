8cfc5a87b6b90e461704af66740f4b6c6f78f355
Rufus <likeness14@gmail.com>
2024-07-24 19:34:21 +0100

    1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,16 +1,16 @@
 function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-        if (success) {
-            resolve({
-                status: 200,
-                body: 'Success',
-              });
-        } else {
-            const msg = 'The fake API is not working currently'
-            const err = new Error(msg);
-            reject(err);
-        }
-    });
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      const msg = 'The fake API is not working currently';
+      const err = new Error(msg);
+      reject(err);
+    }
+  });
 }
 
 export default getFullResponseFromAPI;
