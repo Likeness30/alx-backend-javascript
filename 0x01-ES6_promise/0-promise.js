2ff0a4e232251aafd6fab4a5583f90ec0f0c23fc
Rufus <likeness14@gmail.com>
2024-07-24 17:29:39 +0100

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,17 +1,12 @@
-// Define the function to return a promise
 function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     // Simulate an asynchronous operation
-    const success = true; // Change this to false to simulate a failure
-
-    if (success) {
-      resolve('Success');
-    } else {
-      // eslint-disable-next-line prefer-promise-reject-errors
-      reject('Error');
-    }
+    setTimeout(() => {
+      // For instance lets resolve the promise with some data
+      const data = { message: 'API call sucessful' };
+      resolve(data);
+    }, 1000);
   });
 }
 
-// Export the function as the default export
 export default getResponseFromAPI;
