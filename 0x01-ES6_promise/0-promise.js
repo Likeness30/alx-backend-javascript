9e0a88a61843cbb4ccee467cbd3ad114451a83d7
Rufus <likeness14@gmail.com>
2024-07-24 16:49:03 +0100

    0-promise.js

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,12 @@
+function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    // Simulate an asynchronous operation
+    setTimeout(() => {
+      // For instance lets resolve the promise with some data
+      const data = { message: 'API call sucessful' };
+      resolve(data);
+    }, 1000);
+  });
+}
+
+export default getResponseFromAPI;
