48fdb44589c91015f1cdc0bcdb5108ec5e443aa4
Rufus <likeness14@gmail.com>
2024-07-25 04:18:15 +0100

    4-user-promise.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,12 @@
+// Let's first of all, import the necessary functions from utils.js
 import { uploadPhoto, createUser } from './utils';
 
-/**
- * Simulate a signup process
- */
-export default async function handleProfileSignup() {
-  try {
-    const values = await Promise.all([uploadPhoto(), createUser()]);
-    console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
-  } catch (error) {
-    console.log('Signup system offline');
-  }
+export default function handleProfileSignup() {
+  const photo = uploadPhoto();
+  const user = createUser();
+
+  return Promise.all([photo, user]).then((res) => {
+    console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+  })
+    .catch(() => { console.log('Signup system offline'); });
 }
