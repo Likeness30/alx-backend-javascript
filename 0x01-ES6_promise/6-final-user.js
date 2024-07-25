63ed0a210fabbeb19ee05a9df0bc92d0eda1b853
Rufus <likeness14@gmail.com>
2024-07-25 15:26:04 +0100

    fixed 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,24 +1,19 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-/**
- * Asynchronously handles profile signup by signing up a user and uploading a photo.
- * @param {string} firstName - The first name of the user.
- * @param {string} lastName - The last name of the user.
- * @param {string} fileName - The name of the file to upload.
- * @returns {Promise<Array<{status: string, value: any}>>} An array of objects
- * containing the status and value of each operation. The status is either
- * 'fulfilled' or 'rejected'.
- */
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const handlePromise = (promise) => promise
-    .then((result) => ({ status: 'fulfilled', value: result }))
-    .catch((error) => ({ status: 'rejected', error: `Error: ${error.message}` }));
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
 
-  const promises = [
-    handlePromise(signUpUser(firstName, lastName)),
-    handlePromise(uploadPhoto(fileName)),
-  ];
-
-  return Promise.all(promises);
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then((values) => {
+    const result = [];
+    values.forEach((element) => {
+      if (element.status === 'fulfilled') {
+        result.push({ status: element.status, value: element.value });
+      } else {
+        result.push({ status: element.status, value: `${element.reason}` });
+      }
+    });
+    return result;
+  });
 }
