bb80e84d00ce744e898a95bb3b1f9d400136da4f
Rufus <likeness14@gmail.com>
2024-07-25 13:49:28 +0100

    fix

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,14 +1,72 @@
+// export default function getFullResponseFromAPI(success) {
+//   return new Promise((resolve, reject) => {
+//     if (success) {
+//       resolve({
+//         status: 200,
+//         body: 'Success',
+//       });
+//     } else {
+//       const msg = 'The fake API is not working currently';
+//       const err = new Error(msg);
+//       reject(err);
+//     }
+//   });
+// }
+
+//   .then((result) => {
+//     console.log(result);
+//     return result;
+//   }).catch((err) => {
+//     console.log(err);
+//     return err;
+//   });
+// } else {
+//   new Promise((resolve, reject) => {
+//     reject(new Error('The fake API is not working currently'));
+//   }).catch((error) => {
+//     console.log(error);
+//     return error;
+//   });
+//   // }
+// }
+
+// // export {getFullResponseFromAPI}
+
+// // Promises upon promises
+// function getFullResponseFromAPI(success) {
+//   return new Promise((resolve, reject) => {
+//     if (success) {
+//       resolve({
+//         status: 200,
+//         body: 'Success',
+//       });
+//     } else {
+//       reject(new Error('The fake API is not working currently'));
+//     }
+//   });
+// }
+
+// export default getFullResponseFromAPI;
+
 export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
+  const myPromise = new Promise((resolve, reject) => {
+    if (success === true) {
       resolve({
         status: 200,
         body: 'Success',
       });
     } else {
-      const msg = 'The fake API is not working currently';
-      const err = new Error(msg);
-      reject(err);
+      reject(new Error('The fake API is not working currently'));
     }
   });
+  myPromise.then((result) => {
+    console.log(myPromise);
+    return result;
+  }).catch((err) => {
+    console.log(myPromise);
+    return err;
+  });
 }
+
+getFullResponseFromAPI(true);
+getFullResponseFromAPI(false);
